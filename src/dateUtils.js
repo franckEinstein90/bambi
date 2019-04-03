8ceb3f65896e0f2eed3ef1fadae0779ba886c8e1
U-APCA2\Franck Binard <Franck Binard@BNO-L-PC0ANN8B.APCA2.GC.CA>
2019-04-03 12:50:49 -0400

    updated dateUtils

--- src/dateUtils.js
+++ src/dateUtils.js
@@ -2,10 +2,10 @@
 // begin timeSpan namespace //
 //****************************//
 const timeSpan = (function (){
-    const secondSpanMs = 1000, 
+    const secondSpanMs = 1000,
           daySpanMs = secondSpanMs * 60  * 60 * 24,
           monthAfter = function(monthAsDate){
-                return new Date( monthAsDate.getFullYear(), 
+                return new Date( monthAsDate.getFullYear(),
                              monthAsDate.getMonth() + 1, 1);
           };
     return{
@@ -26,18 +26,15 @@ const timeSpan = (function (){
 // begin dateUtils namespace //
 //****************************//
  const dateUtils = (function (){
-    const theMonths = [ "January","February","March", "April"
-                        , "May","June","July", "August"
-                        , "September","October", "November","December"]
-    , dateOptions = {weekday: 'long'
-                    , year: 'numeric'
-                    , month: 'long', day: 'numeric'};
-    let delimiter = "_"
-        , pad0 = function(digit){
-            return digit.toString().padStart(2,'0');
-          };
+    let theMonths = [ "January","February","March", "April" , "May","June","July", "August" , "September","October", "November","December"],
+    dateOptions = {weekday: 'long' , year: 'numeric' , month: 'long', day: 'numeric'},
+    separator = "_",
+    pad0 = function(digit){ return digit.toString().padStart(2,'0'); };
 
 	return{
+    setSeparator: function(sep){
+			separator = sep;
+		},
 		firstDayofMonth: function(theYear,monthIdx){
 		  return new Date(theYear,monthIdx,1).getDay();
 		},
@@ -48,17 +45,17 @@ const timeSpan = (function (){
 		monthIdxToStr: function(monthIdx){
 		  return theMonths[monthIdx];
 		},
-		dayStamp: function(){
-		  if(arguments.length == 0) { //if the function is called without arguments, returns today as dateStamp
-			  let d = new Date();
-			  return dateUtils.dayStamp(d.getFullYear(), d.getMonth(), d.getDate());
-	          }
-		  return [  arguments[0].toString(), 
-                    pad0(arguments[1]+1), 
-                    pad0(arguments[2])  ].join(delimiter);
-		},
+    dayStamp: function(){
+    if(arguments.length == 0) { //if the function is called without arguments, returns today as dateStamp
+      let d = new Date();
+      return dateUtils.dayStamp(d.getFullYear(), d.getMonth(), d.getDate());
+          }
+    return arguments[0].toString() + separator
+      + (arguments[1]+1).toString().padStart(2,'0') + separator
+      + (arguments[2]).toString().padStart(2,'0');
+  },
 		dayStampToDate: function(dayStamp){
-		  let dateParts = dayStamp.split(delimiter);
+		  let dateParts = dayStamp.split(separator);
 		  return new Date(dateParts[0], dateParts[1]-1, dateParts[2]);
 		},
 		dateToDayStamp: function(someDate){
--- test/dateUtils.js
+++ test/dateUtils.js
@@ -2,8 +2,16 @@ const expect = require('chai').expect;
 const dateUtils = require('../src/dateUtils.js').dateUtils;
 
 const todayDate = new Date();
-const todayStamp = `${todayDate.getFullYear().toString()}_${(todayDate.getMonth()+1).toString().padStart(2,'0')}_${todayDate.getDate().toString()}`;
+const todayStamp = `${todayDate.getFullYear().toString()}_${(todayDate.getMonth()+1).toString().padStart(2,'0')}_${todayDate.getDate().toString().padStart(2,'0')}`;
 
+describe('setSeparator', function(){
+	it('should set the separator for datestamps', function(){
+		dateUtils.setSeparator("-");
+		expect(dateUtils.dayStamp(2980,06,12)).to.be.equal("2980-07-12");
+		expect(dateUtils.dayStamp(1980,6, 2)).to.be.equal("1980-07-02");
+		dateUtils.setSeparator("_");
+	});
+});
 describe('monthLength', function(){
     it('should return the number of days in a month', function(){
         expect(dateUtils.monthLength(2000,0)).to.be.equal(31);
@@ -27,6 +35,9 @@ describe('dayStamp', function(){
 describe('dayStampToDate', function(){
 	it('should return a new date based on a string date stamp in format: yyyy_mm_dd', function(){
 		expect(dateUtils.dayStampToDate("2019_04_01").toDateString()).to.be.equal(new Date(2019, 3, 1).toDateString());
+		dateUtils.setSeparator("-");
+		expect(dateUtils.dayStampToDate("2018-01-04").toDateString()).to.be.equal(new Date(2018, 0, 4).toDateString());
+		dateUtils.setSeparator("_");
 	});
 });
 
@@ -35,6 +46,3 @@ describe('dateToDayStamp', function(){
 		expect(dateUtils.dateToDayStamp(new Date())).to.be.equal(todayStamp);
 	});
 });
-
-	
-
