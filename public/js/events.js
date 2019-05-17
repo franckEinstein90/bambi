3f4cd505701b45cd0b7fdab6f97639aa9853e4c9
U-APCA2\Franck Binard <Franck Binard@BNO-L-PC0ANN8B.APCA2.GC.CA>
2019-05-17 16:30:57 -0400

    added events.Registrar class def

--- public/js/events.js
+++ public/js/events.js
@@ -1,6 +1,7 @@
-const Events = (function() {
-    function generateUUID() {
-        var d = new Date().getTime();
+const events = (function() {
+
+    generateUUID = () => {
+        let d = new Date().getTime();
         if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
             d += performance.now(); //use high-precision timer if available
         }
@@ -11,20 +12,94 @@ const Events = (function() {
         });
     };
     return {
-        Event: function() {
+        eventState: {
+            on: 1,
+            off: 0
+        },
+        Event: function(state) {
             this.id = generateUUID();
-            this.status = "ongoing";
-            this.flip = function() {
-                console.log("flipping status");
-            }
-        }
+            this.state = state ? state : eventState.on;
+        },
+	Registrar: function(){
+	   this.events = new Map();
+	}
     };
 })();
 
-Events.Event.prototype.on = function() {
-    this.status = "ongoing";
+events.Event.prototype = {
+        get isOn() {
+            return (this.state === events.eventState.on)
+        },
+        get isOff() {
+            return (this.state === events.eventState.off)
+        },
+        on: function() {
+            if (this.isOff()) {
+                this.state === events.eventState.on;
+            }
+        },
+        off: function() {
+            if (this.isOn()) {
+                this.state === events.eventState.off;
+            }
+        }
 }
 
-Events.Event.prototype.off = function() {
-    this.status = "offgoing";
-}
+/******************************************************************************
+ * Registrar class
+ * -----------------
+ *  data structure that holds and registers events, 
+ *  keeping track of their status
+ * 
+ * ***************************************************************************/
+events.Registrar.prototype = {
+
+    get size() {
+        return this.events.size;
+    },
+
+   /*****************************************************************
+     *  Registers an event in the registrar
+     *  *************************************************************/
+    register: function(ev) {
+        this.events.set(ev.id, ev);
+    },
+
+    flush: function(ev) {
+        return this.events.clear();
+    },
+
+    forEach: function(eventCallbackFunction) {
+        this.events.forEach(eventCallbackFunction);
+    },
+
+    get: function(eventId) {
+        return this.events.get(eventId);
+    },
+
+    filter: function(filterPred) {
+        /********************************************************
+         * returns an array of events filtered as 
+         * per the predicate argument
+         * *****************************************************/
+        let arrayRes = [];
+        this.events.forEach((value, key) => {
+            if (filterPred(value)) {
+                arrayRes.push(value);
+            }
+        });
+        return arrayRes;
+    },
+    remove: function(evId) {
+        /********************************************************
+         * removes an event with given id from 
+         * the registrar
+         * *****************************************************/
+        if (!this.events.has(evId)) {
+            throw new events.Exception("Event does not exist");
+        }
+        this.events.delete(evId);
+    }
+
+
+};
--- public/js/ui.js
+++ public/js/ui.js
@@ -3,226 +3,273 @@
  * FranckEinstein90
  * -------------------
  *  includes: 
+ *  - pageContainer
  *  - eventDialogController
  *  - eventViewUI
  *  - calendarUI
  * ***************************************************************************/
 
+const pageContainer = (function() {
 
-
-/****************************************************
- * Module eventDialogController
- ***************************************************/
-const eventDialogController = (function() {
-
-    let dialogAction = undefined,
-        eventBeginDateField = "#event-dialog-begin-date",
-        eventEndDateField = "#event-dialog-end-date",
-        dateStampToDate = function(dayStamp) {
-            dateUtils.setSeparator("-");
-            return dateUtils.dayStampToDate(dayStamp)
-        },
-
-        calendarBlankEvent = function(dayStamp) {
-            let beginDate = dateStampToDate(dayStamp);
-            return new eventUtils.Event(beginDate, beginDate, "", "");
-        },
-
-        setEventFormValues = function(ev) {
-            dateUtils.setSeparator('-');
-            AJS.$(eventBeginDateField).val(dateUtils.dateToDayStamp(ev.beginDate));
-            AJS.$(eventEndDateField).val(dateUtils.dateToDayStamp(ev.endDate));
-            if (ev.eventTitle) {
-                AJS.$("#event-dialog-title").val(ev.eventTitle);
-            }
-            if (ev.eventDescription) {
-                AJS.$("#event-dialog-description").val(ev.eventDescription);
-            }
-        },
-
-        setEventDialogHeader = function(headerTitle) {
-            AJS.$("#event-dialog-action").text(headerTitle);
-        },
-
-        validateFormInfo = function() {
-            let fieldAsDate = function(fieldID) {
-                    return dateUtils.dayStampToDate(AJS.$(fieldID).val());
+            let calendar = new events.Registrar(),
+                evRegExp = /Event from (\d{4}\-\d{2}\-\d{2}) to (\d{4}\-\d{2}\-\d{2})\:\s(.+)/,
+                matchEvDescription = (evStr) => {
+                    let found = evStr.match(evRegExp);
+                    if (found) {
+                        return {
+                            beginDate: found[1],
+                            endDate: found[2],
+                            textDescription: found[3]
+                        }
+                    }
                 },
-                beginDate = fieldAsDate(eventBeginDateField),
-                endDate = fieldAsDate(eventEndDateField);
-            console.log("validating");
-        }
+                extractFields = (evInfoStr) => {
+                  return {
+                   beginDate: "a", 
+                   endDate: "b", 
+                   title: "d", 
+                   description: "d"};
+              }, 
 
-    return {
+                processDescription = (calendar, evInfo) => {
+                    try {
+                        let {
+                            beginDate,
+                            endDate,
+                            title,
+                            description
+                        } = extractFields(evInfo); 
+                        calendar.register(
+                                new eventUtils.CalendarEvent(
+                                    beginDate,
+                                    endDate,
+				                            title, 
+                                    description));
+                    } catch (e) {
+                        throw e + "at pageContainer.processDescription"
+                    }
+              };
 
-        dialogActions: {
-            create: 1,
-            edit: 2
-        },
+                    return {
+                        onReady: (eventStrings) => {
+                            eventStrings.forEach(str => processDescription(calendar, str));
+                            calendarUI.onReady(calendar);
+                        }
+                    }
+                })();
 
-        getDialogAction: function() {
-            return dialogAction;
-        },
+        /****************************************************
+         * Module eventDialogController
+         ***************************************************/
+        const eventDialogController = (function() {
 
-        showEdit: function(evID) {
-            dialogAction = eventDialogController.dialogActions.edit;
-            setEventDialogHeader("Modifying existing event");
-            let ev = eventUtils.get(evID);
-            setEventFormValues(ev);
-            AJS.dialog2("#event-dialog").show();
-        },
+            let dialogAction = undefined,
+                eventBeginDateField = "#event-dialog-begin-date",
+                eventEndDateField = "#event-dialog-end-date",
+                dateStampToDate = function(dayStamp) {
+                    dateUtils.setSeparator("-");
+                    return dateUtils.dayStampToDate(dayStamp)
+                },
 
-        showNew: function(dayStamp) {
-            dialogAction = eventDialogController.dialogActions.create;
-            setEventDialogHeader("Creating new event");
-            let ev = calendarBlankEvent(dayStamp);
-            setEventFormValues(ev);
-            AJS.dialog2("#event-dialog").show();
-        },
+                calendarBlankEvent = function(dayStamp) {
+                    let beginDate = dateStampToDate(dayStamp);
+                    return new eventUtils.Event(beginDate, beginDate, "", "");
+                },
 
-        publish: function(ev) {
-            console.log("publishing event to page" + ev.eventTitle);
-            validateFormInfo();
-        }
-    }
-})();
+                setEventFormValues = function(ev) {
+                    dateUtils.setSeparator('-');
+                    AJS.$(eventBeginDateField).val(dateUtils.dateToDayStamp(ev.beginDate));
+                    AJS.$(eventEndDateField).val(dateUtils.dateToDayStamp(ev.endDate));
+                    if (ev.eventTitle) {
+                        AJS.$("#event-dialog-title").val(ev.eventTitle);
+                    }
+                    if (ev.eventDescription) {
+                        AJS.$("#event-dialog-description").val(ev.eventDescription);
+                    }
+                },
 
-const eventViewUI = (function(){
-})();
+                setEventDialogHeader = function(headerTitle) {
+                    AJS.$("#event-dialog-action").text(headerTitle);
+                },
 
-const calendarSettings = (function() {
-    let today = new Date(),
-        month = today.getMonth(),
-        year = today.getFullYear();
-    
-
-    return {
-	    beginYear : function(){
-		    return 2010;
-	    },
-	    endYear: function(){
-		    return 2030;
-	    },
-	getMonth: month, 
-        setMonth: function(y, m) {
-            year = y;
-            month = m;
-        },
-        nextMonth: function() {
-            let m, y;
-            if (month < 11) {
-                m = month + 1;
-                y = year;
-            } else {
-                m = 0;
-                y = year + 1;
-            }
-            calendarSettings.setValues(y, m);
-        },
-        previousMonth: function() {
-            let m, y;
-            if (month > 0) {
-                m = month - 1;
-                y = year;
-            } else {
-                m = 11;
-                y = year - 1;
-            }
-            calendarSettings.setValues(y, m);
-        },
-	yearIdx: function(){
-		return year - calendarSettings.beginYear();
-	},
-        getYear: function() {
-            return year;
-        },
-        firstDay: function() {
-            return dateUtils.firstDayOfMonth(year, month);
-        },
-
-	dayOfWeek: function(monthDay){
-	    //returns the weekday idx (0 to 6 - Sunday to Saturday)
-	    //for the given date 
-	    let d = new Date(year, month, monthDay);
-	    return d.getDay();
-	},
-	firstDayOfMonth : dateUtils.firstDayOfMonth(year, month), 
-	monthLength : dateUtils.monthLength(year, month), 
-        setValues: function(y, m) {
-            if (arguments.length == 0) {
-                let today = new Date();
-                calendarSettings.setValues(today.getFullYear(), today.getMonth());
-                return;
-            }
-            month = m;
-            year = y;
-        }
-    };
-})();
-
-const calendarUI = (function() {
-    let dayIds = [], 
-	calendarTableTitle,
-	tag = (t, content, id) => `<${t}> ${content} </${t}>`,
-        setCalendarTitle = function() {
-            calendarTableTitle =
-                dateUtils.monthIdxToStr(calendarSettings.getMonth) +
-                " " + calendarSettings.getYear();
-            AJS.$("#tableCalendar-title").html("<h1 style='color:white'>" +
-                calendarTableTitle + "</h1>");
-        },
-
-        addTableCells = function(weekRow) {
-            let dayID = function(dayDate) {
-                return dateUtils.dayStamp(
-                    calendarSettings.year,
-                    calendarSettings.month,
-                    dayDate)
-            };
-            return weekRow.map(x => tag('td', dayID(x))).join();
-        },
-
-        addTableBody = function() {
-            let firstWeekday = calendarSettings.firstDay(),
-                currentDay = 1,
-                lastMonthDay = calendarSettings.monthLength;
-	    //empty the dayIds array
-	    dayIds.length = 0;
-	
-            while (currentDay < lastMonthDay) {
-                currentDay = addWeekRow(currentDay);
-            }
-        },
-
-        addWeekRow = function(currentDay) {
-	    let weekRow = "", 
-	        weekDayCell = (day) => {
-			weekRow += `<td>${day === undefined?"--":day}</td>`;
-			return day;
-		}, 
-            newWeekRow = dateUtils.weekDays.map((dayName, dayIdx) => {
-                //has the month begun yet?
-                if (calendarSettings.dayOfWeek(currentDay) > dayIdx) {
-                   weekDayCell();
-                } else if (currentDay > calendarSettings.monthLength) { //has the month already ended?
-		   weekDayCell();
-                } else { //make a regular week day cell
-                    let dayID = weekDayCell(currentDay);
-                    currentDay += 1; //increment current day
-                    dayIds.push(dayID)
+                validateFormInfo = function() {
+                    let fieldAsDate = function(fieldID) {
+                            return dateUtils.dayStampToDate(AJS.$(fieldID).val());
+                        },
+                        beginDate = fieldAsDate(eventBeginDateField),
+                        endDate = fieldAsDate(eventEndDateField);
+                    console.log("validating");
                 }
-            });
 
-            return currentDay
-        }
+            return {
 
-    return {
-        /************************************************************
-         * builds the calendar user interface on the page
-         * **********************************************************/
-        constructCalendarUI: function() {
-            setCalendarTitle();
-            addTableBody();
-        }
-    };
-})();
+                dialogActions: {
+                    create: 1,
+                    edit: 2
+                },
+
+                getDialogAction: function() {
+                    return dialogAction;
+                },
+
+                showEdit: function(evID) {
+                    dialogAction = eventDialogController.dialogActions.edit;
+                    setEventDialogHeader("Modifying existing event");
+                    let ev = eventUtils.get(evID);
+                    setEventFormValues(ev);
+                    AJS.dialog2("#event-dialog").show();
+                },
+
+                showNew: function(dayStamp) {
+                    dialogAction = eventDialogController.dialogActions.create;
+                    setEventDialogHeader("Creating new event");
+                    let ev = calendarBlankEvent(dayStamp);
+                    setEventFormValues(ev);
+                    AJS.dialog2("#event-dialog").show();
+                },
+
+                publish: function(ev) {
+                    console.log("publishing event to page" + ev.eventTitle);
+                    validateFormInfo();
+                }
+            }
+        })();
+
+        const eventViewUI = (function() {})();
+
+        const calendarSettings = (function() {
+            let today = new Date(),
+                month = today.getMonth(),
+                year = today.getFullYear();
+
+
+            return {
+                beginYear: function() {
+                    return 2010;
+                },
+                endYear: function() {
+                    return 2030;
+                },
+                getMonth: month,
+                setMonth: function(y, m) {
+                    year = y;
+                    month = m;
+                },
+                nextMonth: function() {
+                    let m, y;
+                    if (month < 11) {
+                        m = month + 1;
+                        y = year;
+                    } else {
+                        m = 0;
+                        y = year + 1;
+                    }
+                    calendarSettings.setValues(y, m);
+                },
+                previousMonth: function() {
+                    let m, y;
+                    if (month > 0) {
+                        m = month - 1;
+                        y = year;
+                    } else {
+                        m = 11;
+                        y = year - 1;
+                    }
+                    calendarSettings.setValues(y, m);
+                },
+                yearIdx: function() {
+                    return year - calendarSettings.beginYear();
+                },
+                getYear: function() {
+                    return year;
+                },
+                firstDay: function() {
+                    return dateUtils.firstDayOfMonth(year, month);
+                },
+
+                dayOfWeek: function(monthDay) {
+                    //returns the weekday idx (0 to 6 - Sunday to Saturday)
+                    //for the given date 
+                    let d = new Date(year, month, monthDay);
+                    return d.getDay();
+                },
+                firstDayOfMonth: dateUtils.firstDayOfMonth(year, month),
+                monthLength: dateUtils.monthLength(year, month),
+                setValues: function(y, m) {
+                    if (arguments.length == 0) {
+                        let today = new Date();
+                        calendarSettings.setValues(today.getFullYear(), today.getMonth());
+                        return;
+                    }
+                    month = m;
+                    year = y;
+                }
+            };
+        })();
+
+        const calendarUI = (function() {
+            let dayIds = [],
+                calendarTableTitle,
+                tag = (t, content, id) => `<${t}> ${content} </${t}>`,
+                setCalendarTitle = function() {
+                    calendarTableTitle =
+                        dateUtils.monthIdxToStr(calendarSettings.getMonth) +
+                        " " + calendarSettings.getYear();
+                    AJS.$("#tableCalendar-title").html("<h1 style='color:white'>" +
+                        calendarTableTitle + "</h1>");
+                },
+
+                addTableCells = function(weekRow) {
+                    let dayID = function(dayDate) {
+                        return dateUtils.dayStamp(
+                            calendarSettings.year,
+                            calendarSettings.month,
+                            dayDate)
+                    };
+                    return weekRow.map(x => tag('td', dayID(x))).join();
+                },
+
+                addTableBody = function() {
+                    let firstWeekday = calendarSettings.firstDay(),
+                        currentDay = 1,
+                        lastMonthDay = calendarSettings.monthLength;
+                    //empty the dayIds array
+                    dayIds.length = 0;
+
+                    while (currentDay < lastMonthDay) {
+                        currentDay = addWeekRow(currentDay);
+                    }
+                },
+
+                addWeekRow = function(currentDay) {
+                    let weekRow = "",
+                        weekDayCell = (day) => {
+                            weekRow += `<td>${day === undefined?"--":day}</td>`;
+                            return day;
+                        },
+                        newWeekRow = dateUtils.weekDays.map((dayName, dayIdx) => {
+                            //has the month begun yet?
+                            if (calendarSettings.dayOfWeek(currentDay) > dayIdx) {
+                                weekDayCell();
+                            } else if (currentDay > calendarSettings.monthLength) { //has the month already ended?
+                                weekDayCell();
+                            } else { //make a regular week day cell
+                                let dayID = weekDayCell(currentDay);
+                                currentDay += 1; //increment current day
+                                dayIds.push(dayID)
+                            }
+                        });
+
+                    return currentDay
+                }
+
+            return {
+                /************************************************************
+                 * builds the calendar user interface on the page
+                 * **********************************************************/
+                constructCalendarUI: function() {
+                    setCalendarTitle();
+                    addTableBody();
+                }
+            };
+        })();
