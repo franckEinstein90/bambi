29c35fc40e891842f9fec537535b75734eb23103
U-EMERGEDESK\Franck <franckbinard@gmail.com>
2019-05-21 21:02:37 -0400

    separated pageManager from ui.js

--- public/js/ui.js
+++ public/js/ui.js
@@ -3,190 +3,39 @@
  * FranckEinstein90
  * ----------------
  *  includes modules 
- *  - pageContainer: all to do with the page that renders the ui
- *  - eventDialogController: controls the event create and edit dialogs
  *  - eventsUI: all to do with the events view
  *  - calendarUI: all to do with the calendar event
  *
  * ***************************************************************************/
 
 
-/******************************************************************************
- * Utility functions used by all other modules on this 
- * page
- ********************************************************************/
-const p = (function() {
+const ui = (function() {
+
+    let uiHandle = (uiID) => AJS.$("#" + uiID);
+
     return {
-        tag: (t, content, id) => `<${t}> ${content} </${t}>`,
-        makeDiv: (divBody, divId, divClass) => `<div class='${divClass}' id='${divId}'>${divBody}</div>`,
-        makeSpan: (spanBody, spanId, spanClass) => `<span class='${spanClass}' id = '${spanId}'>${spanBody}</span>`,
-        uiHandle: (uiID) => AJS.$("#" + uiID),
-        toDate: (dayStamp) => {
-            let [year, month, day] = dayStamp.split("-");
-            return new Date(year, month - 1, day);
+        assignAction: (cmd, action) => {
+            uiHandle(cmd).click(function(e) {
+                action();
+            })
+        }, 
+        UI: function(uiIds){
+            this.ids = uiIds; 
         }
     }
 })();
 
-
-
-const eventDecoder = (function() {
-    let eventDescriptionMatcher = /Event from (\d{4}\-\d{2}\-\d{2}) to (\d{4}\-\d{2}\-\d{2})\:\s(.+)/,
-        parse = (evStr) => {
-            let recognized = evStr.match(eventDescriptionMatcher);
-            if (recognized) {
-                let [, beginDate, endDate, description] = recognized;
-                return {
-                    beginDate,
-                    endDate,
-                    description
-                }
-            }
-        },
-
-        makeEventObject = (evFieldValues) => {
-            let beginDate = p.toDate(evFieldValues.beginDate),
-            endDate = p.toDate(evFieldValues.endDate),
-            description = evFieldValues.description.split(':').filter(x => x.length > 1);
-
-            try {
-                return new calendarEvents.CalendarEvent(
-                    beginDate,
-                    endDate,
-                    description[0],
-                    description[1]);
-            } catch (e) {
-                throw e + " at eventDecoder.makeEventObject";
-            }
-        }
-
-    return {
-        /********************************************************************
-         * given a string describing a calendar event, verify it for format 
-         *  and content, and if an event can be made of it, create it, id it, 
-         *  and  register it with eventUtils manager
-         ********************************************************************/
-        processEventDescription: (calendar, eventDescriptionString) => {
-            let eventDescriptionFields = parse(eventDescriptionString);
-            if (eventDescriptionFields) {
-                newEvent = makeEventObject(eventDescriptionFields);
-                calendar.register(newEvent);
-            }
-        }
-    }
-})()
-
-
-const pageContainer = (function() {
-
-    let calendar = new events.Registrar();
-
-    return {
-        onReady: (eventStrings) => {
-            try {
-                //process and register all events described on the page 
-                eventStrings.forEach(x => eventDecoder.processEventDescription(calendar, x));
-                //render the various ui elements
-                eventsUI.onReady(calendar);
-                calendarUI.onReady(calendar);
-            } catch (e) {
-                console.log(e);
-            }
-        }
-    };
-})()
-
-
-/****************************************************
- * Module eventDialogController
- * deals with forms to edit or create events
- ***************************************************/
-const eventDialogController = (function() {
-
-    let ui = {
-            eventDialog: "event-dialog",
-            beginDateField: "event-dialog-begin-date",
-            endDateField: "event-dialog-end-date",
-            titleField: "event-dialog-title",
-            descriptionField: "event-dialog-description"
-        },
-
-        showDialog = () => AJS.dialog2("#" + uiElementsIds.eventDialog).show(),
-
-        newUnregisteredEvent = (dayStamp) => {
-            let evDate = toDate(dayStamp);
-            return new calendarUtils.Event(evDate, evDate, "", "");
-        }
-
-
-    return {
-
-        dialogActions: {
-            create: 1,
-            edit: 2
-        },
-
-        showNew: (dayStamp) => { //show event form to create a new event
-            let ev = newUnregisteredEvent(dayStamp);
-            dialogAction = eventDialogController.dialogActions.create;
-            setEventFormValues(ev);
-            showDialog();
-        },
-
-        showEdit: (evID) => { //shows the event form to edit exisiting event
-            let ev = calendarUtils.registered.get(evID);
-            dialogAction = eventDialogController.dialogActions.edit;
-            setEventFormValues(ev);
-            showDialog();
-        }
-    }
-})();
-
-
-
-const eventsUI = (function() {
-
-    let ui = {
-            eventListPane: "eventList"
-
-        },
-
-
-        renderEventsFrame = () => AJS.$("#eventList"),
-
-        renderEventPane = (calendarEvent) => `<h1>${calendarEvent.title}</h1>`,
-
-        renderEventsView = (calendar) => {
-            eventList = renderEventsFrame();
-            calendar.forEach(x => eventList.append(renderEventPane(x)));
-        }
-
-    return {
-
-        onReady: (calendar) => {
-            renderEventsView(calendar);
-        }
-    }
-
-})();
 
 
 const calendarUI = (function() {
 
-    let ui = {
+    let uiCalendar = new ui.UI  ({
             calendarTitle: "tableCalendar-title",
             calendarBody: "tableBody",
             previousMonth: "select-previous-month",
-            nextMonth: "select-next-month"
-        },
-
-        assignAction = (cmd, action) => {
-            p.uiHandle(cmd).click(function(e) {
-                action();
-                setFormValues();
-                renderCalendar();
-            })
-        },
+            nextMonth: "select-next-month",
+            createNewEvent: "dialog-show-button"
+        }),
 
         addCalendarRow = (week) => {
 
@@ -199,19 +48,49 @@ const calendarUI = (function() {
         setTitle = () => {
 
         },
-
+        setFormValues = () => {},
         renderCalendar = () => {
             clear();
             setTitle();
             calendarSettings.weekSpan.forEach(addCalendarRow);
+        }, 
+        assignActions = () => {
+            ui.assignAction(uiCalendar.ids.previousMonth, x => calendarSettings.previousMonth());
+            ui.assignAction(uiCalendar.ids.nextMonth, x => calendarSettings.nextMonth());
+            ui.assignAction(uiCalendar.ids.createNewEvent, x => alert("new event"));
         }
-
-    return {
+      return {
         onReady: (calendar) => {
             calendarSettings.set();
-            assignAction(ui.previousMonth, x => calendarSettings.previousMonth());
-            assignAction(ui.nextMonth, x => calendarSettings.nextMonth());
+            assignActions();
             renderCalendar();
         }
     }
+})();
+
+
+const eventsUI = (function() {
+
+    let ui = {
+            eventlistpane: "eventlist"
+
+        },
+
+
+        rendereventsframe = () => AJS.$("#eventlist"),
+
+        rendereventpane = (calendarevent) => `<h1>${calendarevent.title}</h1>`,
+
+        rendereventsview = (calendar) => {
+            eventlist = rendereventsframe();
+            calendar.forEach(x => eventlist.append(rendereventpane(x)));
+        }
+
+    return {
+
+        onReady: (calendar) => {
+            rendereventsview(calendar);
+        }
+    }
+
 })();
