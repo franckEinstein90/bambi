fd9306c511b501876b8635de12c442ade6d8b0cc
Franck Binard <franckbinard@gmail.com>
2019-09-10 10:46:59 -0400

    Update main.js

--- src/client/main.js
+++ src/client/main.js
@@ -1,21 +1,55 @@
-/*****************************************************************************/
-const bambi = require('../bambi').bambi;
-const pageContainer = require('./pageContainer').pageContainer;
-
-
-/*****************************************************************************
-
-
-*****************************************************************************/
+"use strict"; 
+/****************************************************
+ * Application Entry point
+ * 
+ ****************************************************/
+const bambi = require('../bambi.js').bambi;
+const errors = require('../errors.js').errors;
+const pageContainer = require('./pageContainer.js').pageContainer; 
 
 AJS.toInit(function($) {
+    /*********************************************
+     *
+     * *******************************************/
+    console.log("App Start");
+    jQuery.extend({
+        getValues: function(url) {
+            var result = null;
+            jQuery.ajax({
+                url: url,
+                type: 'get',
+                async: false,
+                success: function(data) {
+                    result = data;
+                }
+            });
+            return result;
+        }
+    });
 
-    console.log("App Entry Point");
     bambi.init();
-  /*********************************************************************
-     * inits and sets-up the varous ui elements
-     * using array of event description as input
-     ********************************************************************/
-    pageContainer.onReady();
-
+    if (!bambi.goodEnv()) {
+        errors.doError({
+            title: "bad setup",
+            body: "Your calendar install is not set up properly." +
+                  " Please contact the Confluence team to resolve this issue and get your calendar properly initialize"
+        });
+    }
+    else{
+        bambi.setEnv();
+        /**********************************************************************
+         * if this is running in a confluence environemnt, bambi expects the 
+         * variable "confEnv" to be defined
+         * ************************************************************************/
+       
+        if(! bambi.isDev()){ 
+            try {
+                confluencePage.onReady(confEnv.pageID);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        pageContainer.onReady();
+    }
 });
+
