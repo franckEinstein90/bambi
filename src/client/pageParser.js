51137f4066a14ca5d3cd01a4c3e00ba1102ac56f
Franck Binard <franckbinard@gmail.com>
2019-09-16 10:17:22 -0400

    Update pageParser.js

--- src/client/pageParser.js
+++ src/client/pageParser.js
@@ -1,94 +1,140 @@
-"use strict"; 
+"use strict";
 /**********************************************
  * pageParser.js
  * Extracts event information stored on the page
  *********************************************/
 const bambi = require('../bambi').bambi
 const calendarEvents = require('../events/calendarEvents').calendarEvents
+const eventCategories = require('../events/eventCategories').eventCategories
 
- const pageParser = (function() {
-    let fieldSeparator, updateMatchers, 
-    calendarVersionInfoReg, 
-    eventDescriptionMatcher, infoType,
-    toDate,
-    parse, tryParseJSON,  makeEventFromStringValues;
-    
-    toDate = (dayStamp) => { 
+const pageParser = (function() {
+    let infoType, //codes for the type of information described
+        fieldSeparator, updateMatchers,
+        calendarVersionInfoReg,
+        eventDescriptionMatcher,
+        toDate,
+        parse, tryParseJSON, parseFromObject,
+        //events
+        registerEventFromStringDescription, registerEventFromJSONDescription,
+        //categories
+        createCategoryFromJSONDescription
+    toDate = (dayStamp) => {
         let [year, month, day] = dayStamp.split("-");
         return new Date(year, month - 1, day);
     }
+
     calendarVersionInfoReg = /Parks Canada Confluence Calendar - v\d\.\d/
-    fieldSeparator = bambi.htmlFieldSeparator();
 
-    tryParseJSON = function(jsonString){
-        try {
-         var o = JSON.parse(jsonString);
+    fieldSeparator = bambi.htmlFieldSeparator()
 
-        // Handle non-exception-throwing cases:
-        // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
-        // but... JSON.parse(null) returns null, and typeof null === "object", 
-        // so we must check for that, too. Thankfully, null is falsey, so this suffices:
-            if (o && typeof o === "object") {
-                return o;
-            }
-        }
-        catch (e) { }
+    tryParseJSON = function(jsonString) { //Description in JSON format
+            try {
+                var o = JSON.parse(jsonString);
+                if (o && typeof o === "object") {
+                    return o;
+                }
+            } catch (e) {}
             return false;
-     },
-    
-    infoType = {
-        JSONEvent: 10, 
-        STRINGEvent: 11
-    },
+        },
 
-    parse = (evStr) => {
-            let recognized, obj; 
-            obj = tryParseJSON(evStr)
-            if(obj) {
-                return {type: infoType.JSONEvent, value:obj}
+        infoType = {
+            JSONCategoryDescription: 10,
+            JSONEvent: 20,
+            STRINGEvent: 30
+        },
+
+        parseFromObject = function(infoObj) {
+            if ('infoType' in infoObj) {
+                switch (infoObj.infoType) {
+                    case infoType.JSONCategoryDescription:
+                        return {
+                            type: infoType.JSONCategoryDescription,
+                            value: infoObj
+                        }
+                        break;
+                }
             }
+            return {
+                type: infoType.JSONEvent,
+                value: infoObj
+            }
+        },
+
+        parse = (evStr) => {
+            let recognized, obj;
+            //is this a JSON description? 
+            obj = tryParseJSON(evStr)
+            if (obj) {
+                return parseFromObject(obj)
+            }
+
+            //string ad-hoc description
             recognized = evStr.match(calendarVersionInfoReg)
-            if(recognized){//version information
-                    let [, vInfo] = recognized
-                    bambi.prevVersion = vInfo
-                    return;  
+            if (recognized) { //version information
+                let [, vInfo] = recognized
+                bambi.prevVersion = vInfo
+                return;
             }
             recognized = evStr.match(eventDescriptionMatcher);
             if (recognized) {
                 let [, beginDate, endDate, description] = recognized;
                 return {
                     type: infoType.STRINGEvent,
-                    value:{
+                    value: {
                         beginDate,
                         endDate,
                         description
                     }
                 }
             }
-    }
-    makeEventFromStringValues = (evFieldValues) => {
-        let beginDate, endDate, description 
+        }
+
+    registerEventFromStringDescription = (calendar, evFieldValues) => {
+        let beginDate, endDate, description
         beginDate = toDate(evFieldValues.beginDate)
         endDate = toDate(evFieldValues.endDate)
         description = evFieldValues.description.split(new RegExp(fieldSeparator)).filter(x => x.length > 1)
 
-            try {
-                return new calendarEvents.CalendarEvent(
+        try {
+            calendar.register(
+                new calendarEvents.CalendarEvent(
                     beginDate,
                     endDate,
                     description[0].trim(),
                     description[1])
-            } catch (e) {
-                throw e + " at eventDecoder.makeEventObject";
-            }
+            )
+
+        } catch (e) {
+            throw e + " at eventDecoder.makeEventObject";
+        }
     }
 
-    return { 
+    registerEventFromJSONDescription = function(calendar, evDescription) {
+        let newCalendarEvent
+        try {
+            newCalendarEvent = calendarEvents.newCalendarEvent({
+                beginDate: evDescription.timeSpan.beginDate,
+                endDate: evDescription.timeSpan.endDate,
+                title: evDescription.eventTitle,
+                description: evDescription.eventDescription,
+                categoryID: evDescription.categoryID || 0
+            })
+            calendar.register(newCalendarEvent)
+        } catch (err) {
+            console.log("unable to create new calendar event from JSON description at processCalendarInformation" + e)
+        }
+    }
+
+    createCategoryFromJSONDescription = function(calendar, categoryDescription) {
+
+    }
+
+    return {
         onReady: function() {
-           pageParser.updateMatchers()
+            pageParser.updateMatchers()
         },
-       
-        updateMatchers : function(){ //used to extract information stored directly on a confluence page
+
+        updateMatchers: function() { //used to extract information stored directly on a confluence page
             eventDescriptionMatcher = new RegExp(`Event from (\\d{4}\\-\\d{2}\\-\\d{2}) to (\\d{4}\\-\\d{2}\\-\\d{2})\\s*${fieldSeparator}\\s*(.+)`)
         },
         /********************************************************************
@@ -96,32 +142,26 @@ const calendarEvents = require('../events/calendarEvents').calendarEvents
          *  and content, and if an event can be made of it, create it, id it,
          *  and  register it with eventUtils manager
          ********************************************************************/
+
+
         processCalendarInformation: (calendar, eventDescriptionString) => {
             let infoDescription = parse(eventDescriptionString);
-            if(!infoDescription){
+            if (!infoDescription) {
                 return
             }
-            if(infoDescription.type === infoType.JSONEvent){
-                    let newCalendarEvent
-                    try{
-                        newCalendarEvent = new calendarEvents.CalendarEvent(
-                            infoDescription.value.timeSpan.beginDate,
-                            infoDescription.value.timeSpan.endDate,
-                            infoDescription.value.eventTitle,
-                            infoDescription.value.eventDescription
-                        )
-                        calendar.register(newCalendarEvent)
-                        return; 
-                    }
-                    catch(e){
-                        console.log("unable to create new calendar event from JSON description at processCalendarInformation" + e)
-                    }
-            }
-            if(infoDescription.type === infoType.STRINGEvent){
-                calendar.register(makeEventFromStringValues(infoDescription.value))
-                return; 
+            switch (infoDescription.type) {
+                case (infoType.JSONEvent):
+                    registerEventFromJSONDescription(infoDescription.value)
+                    break
+                case (infoType.StringEvent):
+                    registerEventFromStringDescription(calendar, infoDescription.value)
+                    break
+                case (infoType.JSONCategoryDescription):
+                    createCategoryFromJSONDescription(calendar, infoDescription.value)
+                    break
             }
         }
+
     }
 })()
 
